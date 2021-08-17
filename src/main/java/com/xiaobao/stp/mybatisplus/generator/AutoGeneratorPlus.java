9cbde3b6ab1397b8aaa3d1b25d5a539053deb20b
庞亚彬 <yabin.pang@xiaobao100.com>
2021-08-17 17:32:22 +0800

    初始化项目

--- src/main/java/com/xiaobao/stp/mybatisplus/generator/AutoGeneratorPlus.java
+++ src/main/java/com/xiaobao/stp/mybatisplus/generator/AutoGeneratorPlus.java
@@ -1,7 +1,6 @@
 package com.xiaobao.stp.mybatisplus.generator;
 
 import com.baomidou.mybatisplus.generator.AutoGenerator;
-import com.baomidou.mybatisplus.generator.config.PackageConfig;
 import com.xiaobao.stp.mybatisplus.convert.MysqlTypeConvertSupport;
 
 /**
@@ -10,13 +9,20 @@ import com.xiaobao.stp.mybatisplus.convert.MysqlTypeConvertSupport;
  */
 public class AutoGeneratorPlus extends AutoGenerator {
 
+    /**
+     * 回调类, 在执行
+     * @see #execute() 方法之前进行调用, 进行默认的 TypeConvert 配置
+     */
+    private void initTypeConvert() {
+        getDataSource().setTypeConvert(new MysqlTypeConvertSupport(getPackageInfo().getParent()));
+    }
+
     @Override
-    public AutoGenerator setPackageInfo(PackageConfig packageInfo) {
-        AutoGenerator generator = super.setPackageInfo(packageInfo);
+    public void execute() {
+        // 设置默认的 TypeConvert
+        initTypeConvert();
 
-        getDataSource().setTypeConvert(new MysqlTypeConvertSupport(packageInfo.getParent()));
-
-        return generator;
+        super.execute();
     }
 
 }
